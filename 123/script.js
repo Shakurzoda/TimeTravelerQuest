4f2cb9260bf68b74880a0e7078e0eca830865c54
Shakurzoda Farahmand <84468052+Shakurzoda@users.noreply.github.com>
2025-01-16 02:44:55 +0300

    1

---
+++ 123/script.js
@@ -0,0 +1,64 @@
+/* let timerInterval;
+let elapsedTime = 0;
+
+const timerDisplay = document.getElementById("timerDisplay");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const resetButton = document.getElementById("resetButton");
+
+function updateDisplay() {
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60;
+  timerDisplay.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    elapsedTime++;
+    updateDisplay();
+  }, 1000);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = false;
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  updateDisplay();
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
+
+updateDisplay();
+ */
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
